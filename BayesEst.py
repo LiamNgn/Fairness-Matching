4e888e428ab185b43b64e2f424f855dc3a495526
LiamNgn <tlam.nguyenle@gmail.com>
2024-09-13 20:34:37 +0200

    Update the analytical version of the conditional expectation

--- BayesEst.py
+++ BayesEst.py
@@ -3,7 +3,7 @@ from scipy.stats import norm
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import quad
-
+from scipy.special import erf
 
 #In the case of Gaussian marginals
 def joint_dist(w,w_a,p_b,chi,sigma):
@@ -25,3 +25,17 @@ def exp_val(w_a,p_b,chi,sigma):
 def bayesian_est(w_a,p_b,chi,sigma):
     return exp_val(w_a,p_b,chi,sigma)[0]/gaussian_denominator(w_a,p_b,chi,sigma)
 
+###Analytically calculate the conditional expectation
+def numerator(w_a,p_b,chi,sigma):
+    a = chi**2/(chi**2+sigma**2)*w_a
+    b = np.sqrt(chi**2*sigma**2/(chi**2+sigma**2))
+    return a/2 + a/2*erf((p_b-a)/(np.sqrt(2*(sigma**2+b**2)))) - (b**2)/(np.sqrt(2*np.pi*(b**2 + sigma**2)))*np.exp(-(p_b-a)**2/(2*(b**2+sigma**2)))
+    
+
+def denominator(w_a,p_b,chi,sigma):
+    mean_cond_dist = chi**2/(chi**2 + sigma**2)*w_a
+    var_cond_dist = (sigma**4+2*sigma**2*chi**2)/(chi**2 + sigma**2)
+    return norm.cdf(p_b,mean_cond_dist,np.sqrt(var_cond_dist))
+
+def anal_cond_exp(w_a,p_b,chi,sigma):
+    return numerator(w_a,p_b,chi,sigma)/denominator(w_a,p_b,chi,sigma)
