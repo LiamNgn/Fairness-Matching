00083441169263d0a8bf01938841ef5f8a802aa8
LiamNgn <tlam.nguyenle@gmail.com>
2024-09-20 17:44:26 +0200

    Minor changes to make it easier to read

--- test.py
+++ test.py
@@ -31,18 +31,18 @@ def objective_bads(params):
     f1,f2 = market_clear(Pa, Pb, grade_estimated, prop_gp[0], capacities_rate[0], capacities_rate[1], prop_all_g_prefer[0], prop_all_g_prefer[1], sigma_i, sigma_ii, cor_i, cor_ii,chi,sigma,bayes='right')
     # return np.abs(f1) + np.abs(f2)
     return f1**2+f2**2
+# %%
 lower_bounds = np.array([-10, -10])
 upper_bounds = np.array([10, 10])
 plausible_lower_bounds = np.array([-5, -5])
 plausible_upper_bounds = np.array([5, 5])
 x0 = np.array([0, 0]);        # Starting point
+# %%
 bads = BADS(objective_bads, x0, lower_bounds, upper_bounds, plausible_lower_bounds, plausible_upper_bounds)
 optimize_result = bads.optimize()
-x_min = optimize_result['x']
-fval = optimize_result['fval']
-
-print(f"BADS minimum at: x_min = {x_min.flatten()}, fval = {fval:.4g}")
-print(f"total f-count: {optimize_result['func_count']}, time: {round(optimize_result['total_time'], 2)} s")
 
 # %%
-print(capacities_rate)
+x_min = optimize_result['x']
+fval = optimize_result['fval']
+print(f"BADS minimum at: x_min = {x_min.flatten()}, fval = {fval:.4g}")
+print(f"total f-count: {optimize_result['func_count']}, time: {round(optimize_result['total_time'], 2)} s")
